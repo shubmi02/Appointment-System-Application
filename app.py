234f491dcac68bedd1b12643ef17f651a21dc1a2
Shubham Mishra <shubham.mishra@sjsu.edu>
2024-10-08 23:24:26 -0700

    registration, login, and authentication complete

--- app.py
+++ app.py
@@ -1,5 +1,31 @@
 from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userdata.db'
+db = SQLAlchemy(app)
+app.secret_key = 'secret_key'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), nullable = False)
+    email = db.Column(db.String(200), nullable = False, unique = True)
+    password = db.Column(db.String(100))
+    def __init__(self, email, password, name):  
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+def __repr__(self):
+        return f"Name: {self.name}, ID: {self.id}, Role: {self.role}"
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def hello_world():
@@ -7,8 +33,30 @@ def hello_world():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        new_user = User(name = name, email = email, password = generate_password_hash(password))
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/login')
+
     return render_template('register.html')
 
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.check_password(password):
+            session['name'] = user.name
+            session['email'] = user.email
+            return redirect('/')
+        else:
+            return render_template('login.html', error="Invalid username or password")
+
     return render_template('login.html')
